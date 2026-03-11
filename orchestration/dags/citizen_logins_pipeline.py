e6a042fdaa9773328652d21807b857d561211913
Gailey Leseman <gaileyleseman@gmail.com>
2026-03-11 14:08:20 +0100

    add ingestion step and dbt - as is

--- orchestration/dags/citizen_logins_pipeline.py
+++ orchestration/dags/citizen_logins_pipeline.py
@@ -10,6 +10,14 @@ _minio_env = {
     "MINIO_BUCKET_RAW": os.environ.get("MINIO_BUCKET_RAW", "raw"),
 }
 
+_postgres_env = {
+    "POSTGRES_HOST": os.environ.get("POSTGRES_HOST", "postgres"),
+    "POSTGRES_PORT": os.environ.get("POSTGRES_PORT", "5432"),
+    "POSTGRES_DB": os.environ.get("POSTGRES_DB", "hackathon"),
+    "POSTGRES_USER": os.environ.get("POSTGRES_USER", "hackathon"),
+    "POSTGRES_PASSWORD": os.environ.get("POSTGRES_PASSWORD", "hackathon"),
+}
+
 with DAG(
     dag_id="citizen_pipeline",
     schedule="@daily",
@@ -21,17 +29,47 @@ with DAG(
     scrape_citizens = DockerOperator(
         task_id="scrape_citizens",
         image="airflow-hackathon-scraper-citizens",
-        environment={**_minio_env, "PIPELINE_DATE": "{{ (data_interval_start or dag_run.run_after) | ds }}"},
+        environment={
+            **_minio_env,
+            "PIPELINE_DATE": "{{ (data_interval_start or dag_run.run_after) | ds }}",
+        },
         network_mode="hackathon-network",
         auto_remove="force",
+        mount_tmp_dir=False,
         docker_url="unix://var/run/docker.sock",
     )
 
     scrape_logins = DockerOperator(
         task_id="scrape_logins",
         image="airflow-hackathon-scraper-logins",
-        environment={**_minio_env, "PIPELINE_DATE": "{{ (data_interval_start or dag_run.run_after) | ds }}"},
+        environment={
+            **_minio_env,
+            "PIPELINE_DATE": "{{ (data_interval_start or dag_run.run_after) | ds }}",
+        },
         network_mode="hackathon-network",
         auto_remove="force",
+        mount_tmp_dir=False,
         docker_url="unix://var/run/docker.sock",
     )
+
+    ingest = DockerOperator(
+        task_id="ingest",
+        image="airflow-hackathon-ingestion",
+        environment={**_minio_env, **_postgres_env},
+        network_mode="hackathon-network",
+        auto_remove="force",
+        mount_tmp_dir=False,
+        docker_url="unix://var/run/docker.sock",
+    )
+
+    dbt_transform = DockerOperator(
+        task_id="dbt_transform",
+        image="airflow-hackathon-transform",
+        environment=_postgres_env,
+        network_mode="hackathon-network",
+        auto_remove="force",
+        mount_tmp_dir=False,
+        docker_url="unix://var/run/docker.sock",
+    )
+
+    [scrape_citizens, scrape_logins] >> ingest >> dbt_transform
